749adc1a2ebeb437c9e3a4c4e97f97a53626132b
Henry Morin <tronlover101@gmail.com>
2022-06-15 15:30:43 -0500

    Create talkingwalls.ino

---
+++ talkingwalls/talkingwalls.ino
@@ -0,0 +1,92 @@
+/*
+  Controlling the Qwiic MP3 Trigger with I2C Commands
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: January 12th, 2019
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+  This example plays the first track on the SD card.
+
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun! https://www.sparkfun.com/products/15165
+
+  Hardware Connections:
+  Plug in headphones
+  Make sure the SD card is in the socket and has some MP3s in the root directory
+  Don't have a USB cable connected to the Qwiic MP3 Trigger right now
+  If needed, attach a Qwiic Shield to your Arduino/Photon/ESP32 or other
+  Plug the Qwiic device onto an available Qwiic port
+  Open the serial monitor at 115200 baud
+*/
+
+#include <Wire.h> //Needed for I2C to Qwiic MP3 Trigger
+
+#include "SparkFun_Qwiic_MP3_Trigger_Arduino_Library.h" //http://librarymanager/All#SparkFun_MP3_Trigger
+
+#define enablePin 3
+#define volumeDPin 4
+#define volumeUPin 5
+
+
+MP3TRIGGER mp3;
+
+String num = "1";
+int vol = "10";
+
+
+void setup()
+{
+  pinMode(enablePin, INPUT);
+  pinMode(volumeDPin, INPUT);
+  pinMode(volumeUPin, INPUT);
+  
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  delay(2000);
+
+  //Check to see if Qwiic MP3 is present on the bus
+  if (mp3.begin() == false)
+  {
+    Serial.println("Qwiic MP3 failed to respond. Please check wiring and possibly the I2C address. Freezing...");
+    while (1)
+      ;
+  }
+
+  mp3.setVolume(10); //Volume can be 0 (off) to 31 (max)
+
+  
+
+  mp3.playTrack(1); //Begin playing the first track on the SD card
+
+  Serial.println("All done!");
+}
+
+void loop()
+{
+  
+  while (mp3.isPlaying() == true)
+    {
+      
+      if (digitalRead(enablePin) == HIGH){
+        Serial.println("on");
+        mp3.setVolume(15);
+      }
+      else if (digitalRead(enablePin) == LOW){
+        Serial.println("off");
+        mp3.setVolume(0);
+      }
+      delay(50); // Don't pound the Qwiic MP3 too hard!
+    }
+  if (num == "1"){
+    num = "2";
+    mp3.playNext();
+    delay(50);
+  }
+  if (num == "2"){
+    num = "1";
+    mp3.playPrevious();
+    delay(50);
+  }
+}
